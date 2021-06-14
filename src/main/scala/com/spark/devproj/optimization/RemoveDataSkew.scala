6fc6f4459253cf1b51b5c197b8a61a9ea739dd47
Harikrishna Thariboyina <=>
2021-06-14 16:49:09 +0530

    new examples

---
+++ src/main/scala/com/spark/devproj/optimization/RemoveDataSkew.scala
@@ -0,0 +1,64 @@
+package com.spark.devproj.optimization
+
+//https://www.youtube.com/watch?v=d41_X78ojCg
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{array, concat, explode, floor, lit, rand}
+
+object RemoveDataSkew {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf();
+    sparkConf.setMaster("local[*]")
+      .setAppName("RemoveDataSkewness");
+
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate();
+    //
+    //    import spark.implicits._
+    //
+    //    //DataFrame 1
+    //    val df1 = Seq(("x", "bc"),
+    //      ("x", "ce"),
+    //      ("x", "ab"),
+    //      ("x", "ef"),
+    //      ("x", "gh"),
+    //      ("y", "hk"),
+    //      ("z", "jk")).toDF()
+    //    df1.show()
+    //    df1.show(10, false)
+    //
+    //    //DataFrame2
+    //    val df2 = Seq(
+    //      ("x", "gkl"),
+    //      ("y", "nmb"),
+    //      ("z", "qwe")
+    //    ).toDF()
+    //
+    //    df2.show(10, false)
+    //    val (df3, df4) = elimnateDataSkew(df1, "_1", df2)
+    //
+    //    // Method to eliminate data skewness
+    //    def elimnateDataSkew(leftTable: DataFrame, leftCol: String, rightTable: DataFrame) = {
+    //
+    //      var df1 = leftTable
+    //        .withColumn(leftCol, concat(
+    //          leftTable.col(leftCol), lit("_"), lit(floor(rand(123456) * 10))))
+    //      var df2 = rightTable
+    //        .withColumn("explodedCol",
+    //          explode(
+    //            array((0 to 10).map(lit(_)): _ *)
+    //          ))
+    //
+    //      (df1, df2)
+    //    }
+    //
+    //    df3.show(100, false)
+    //    df4.show(100, false)
+    //
+    //    //join after elminating data skewness
+    //    df3.join(
+    //      df4,
+    //      df3.col("_1") <=> concat(df4.col("_1"), lit("_"), df4.col("explodedCol")))
+    //      .show(100, false)
+  }
+}
---
+++ src/main/scala/com/spark/examples/rdd/rddCreation.scala
@@ -0,0 +1,20 @@
+package com.spark.examples.rdd
+
+import org.apache.hadoop.hive.ql.optimizer.spark.SparkSkewJoinProcFactory.SparkSkewJoinJoinProcessor
+import org.apache.spark.sql.SparkSession
+
+object rddCreation {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("Rdd Creation")
+      .getOrCreate()
+    val dataSeq = Seq(("Java", 20000), ("Python", 3000))
+    val rdd = spark.sparkContext.parallelize(dataSeq)
+    System.out.println("defaultParallelism " +spark.sparkContext.defaultParallelism)
+    System.out.println("defaultMinPartitions " +spark.sparkContext.defaultMinPartitions)
+    val rddPartitions = rdd.getNumPartitions;
+    System.out.println("Rdd Partitions ::"+rddPartitions);
+    scala.io.StdIn.readLine()
+  }
+}
